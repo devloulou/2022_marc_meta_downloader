53484a46001fc90e9ccdfd69b5f539fedb212fd1
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-05-27 20:37:39 +0200

    implement search wrapper module

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pprint
 import tmdbsimple as tmdb
+from urllib.request import urlopen
 
 tmdb.API_KEY = "454b6ca4172e455fe7a7d8395c10d6d9"
 
@@ -7,6 +8,13 @@ image_path_string = 'https://image.tmdb.org/t/p/original/bk9GVjN4kxmGekswNigaa5Y
 
 search = tmdb.Search()
 
-movie_data = search.movie(query="Alien")
+movie_data = search.movie(query="The")
 
-pprint.pprint(movie_data['results'][0], indent=4)
+pprint.pprint(movie_data['results'], indent=4)
+
+# temp = urlopen(image_path_string).read()
+
+# print(temp)
+
+# with open('alien.jpg', "wb") as poster:
+#     poster.write(urlopen(image_path_string).read())
--- utils/search_wrapper.py
+++ utils/search_wrapper.py
@@ -1,3 +1,36 @@
-import tmdbsimple
+from urllib.request import urlopen
+import tmdbsimple as tmdb
 
-# csak a teszt kedvéért
+class SearchWrapper:
+    tmdb.API_KEY = "454b6ca4172e455fe7a7d8395c10d6d9"
+
+    def __init__(self):
+        self.image_api = 'https://image.tmdb.org/t/p/original'
+        self.search = tmdb.Search()
+        self.jpg_path = None
+
+    def get_movie_data(self, movie):
+        data = self.search.movie(query=movie)['results'][0]
+        self.jpg_path = data['poster_path']
+        return data
+
+    def get_image_object(self, jpg_path=None):
+        # if jpg_path is None:
+        #     url = self.image_api + self.jpg_path
+        # else:
+        #     url = self.image_api + jpg_path
+        
+        # return urlopen(url)
+
+        url = self.image_api + self.jpg_path if jpg_path is None \
+                else self.image_api + jpg_path
+
+        return urlopen(url)
+
+if __name__ == '__main__':
+    my_search = SearchWrapper()
+    data = my_search.get_movie_data('Alien')
+    image_link = my_search.get_image_object()
+
+    with open('alien.jpg', "wb") as poster:
+        poster.write(image_link.read())

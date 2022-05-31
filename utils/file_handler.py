1b33333e2ed9d4335193142f9af899aa3d1c3844
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-05-31 19:00:25 +0200

    implement json writer function

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -13,10 +13,24 @@ class FileHandler:
         # Alien.mkv -> ['Alien', 'mkv']
         return [item for item in os.listdir(folder_path) if item.split('.')[-1] in self.allowed_formats]
 
-    
+    @staticmethod
+    def write_json(json_path, data):
+        if os.path.exists(os.path.dirname(json_path)):
+            # ha létezik a json kiírásnál a json-t tartalmazó mappa, akkor mehet az írás
+            try:
+                with open(json_path, "w", encoding="utf-8") as f:
+                    json.dump(data, f, ensure_ascii=False, indent=4)
+            except Exception as e:
+                return False, str(e)
+        
+        return True
+
+
 
 if __name__ == '__main__':
     handler = FileHandler()
 
     file_list = handler.get_file_list()
     print(file_list)
+
+    handler.write_json(None, None)

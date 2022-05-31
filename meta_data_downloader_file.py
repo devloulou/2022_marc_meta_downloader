806f0125cd692c4e7234b5073910d51dae494e2c
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-05-31 20:33:23 +0200

    delete not used meta and refactoring

--- meta_data_downloader_file.py
+++ meta_data_downloader_file.py
@@ -3,14 +3,16 @@ from utils.file_handler import FileHandler
 from utils.search_wrapper import SearchWrapper
 from utils.parameters import movie_data, poster_path
 
-def loader():
-    file_handler = FileHandler()
+def loader(file_handler):    
     search = SearchWrapper()
 
-    movie_list = file_handler.get_file_list()
+    movie_list = [item.split('.')[0] for item in file_handler.get_movies()]
+    meta_data = [item.split('.')[0] for item in file_handler.get_files_from_folder(movie_data)]
 
-    for item in movie_list:
-        movie_name = item.split('.')[0]
+    need_to_download = [item for item in movie_list if item not in meta_data]
+
+    for item in need_to_download:
+        movie_name = item
         meta_data = search.get_movie_data(movie_name)
 
         image_link = search.get_image_object()
@@ -21,5 +23,28 @@ def loader():
         file_handler.write_json(json_path, meta_data)
         file_handler.write_image(image_path, image_link)
 
+
+def delete_unnecessary_meta(file_handler):
+    movie_list = [item.split('.')[0] for item in file_handler.get_movies()]
+
+    meta_data = [item.split('.')[0] for item in file_handler.get_files_from_folder(movie_data)]
+
+    need_to_delete = [item for item in meta_data if item not in movie_list]
+
+    for item in need_to_delete:        
+        image_path = os.path.join(poster_path, f"{item}.jpeg")
+        meta_path = os.path.join(movie_data, f"{item}.json")
+
+        os.remove(image_path)
+        os.remove(meta_path)
+
+
+def main():
+    file_handler = FileHandler()
+
+    delete_unnecessary_meta(file_handler)
+    loader(file_handler)
+
+
 if __name__ == '__main__':
-    loader()
+    main()
